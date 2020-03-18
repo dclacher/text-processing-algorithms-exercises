ae82631c799ef3361a8e3c23d4b69d34ef475f56
Daniel Lacher <carneird@uwindsor.ca>
2020-03-18 01:42:24 -0400

    Fixing questions 3 and 4

--- src/main/java/html_to_text/ConversionUtils.java
+++ src/main/java/html_to_text/ConversionUtils.java
@@ -1,12 +1,7 @@
 package html_to_text;
 
 import org.jsoup.Jsoup;
-import org.jsoup.internal.StringUtil;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Node;
-import org.jsoup.nodes.TextNode;
-import org.jsoup.select.NodeTraversor;
-import org.jsoup.select.NodeVisitor;
 
 import java.io.File;
 import java.io.IOException;
@@ -22,75 +17,10 @@ public class ConversionUtils {
     public static String getPlainTextFromHtml(File file) {
         try {
             Document document = Jsoup.parse(file, "UTF-8", "");
-            FormattingVisitor formatter = new FormattingVisitor();
-            NodeTraversor.traverse(formatter, document); // walk the DOM, and call .head() and .tail() for each node
-            return formatter.toString();
+            return document.text();
         } catch(IOException e) {
             e.printStackTrace();
         }
         return null;
-    }
-
-    // The formatting rules, implemented in a breadth-first DOM traverse
-    private static class FormattingVisitor implements NodeVisitor {
-        private static final int maxWidth = 80;
-        private int width = 0;
-        private StringBuilder accum = new StringBuilder(); // holds the accumulated text
-
-        // hit when the node is first seen
-        public void head(Node node, int depth) {
-            String name = node.nodeName();
-            if (node instanceof TextNode)
-                append(((TextNode) node).text()); // TextNodes carry all user-readable text in the DOM.
-            else if (name.equals("li"))
-                append("\n * ");
-            else if (name.equals("dt"))
-                append("  ");
-            else if (StringUtil.in(name, "p", "h1", "h2", "h3", "h4", "h5", "tr"))
-                append("\n");
-        }
-
-        // hit when all of the node's children (if any) have been visited
-        public void tail(Node node, int depth) {
-            String name = node.nodeName();
-            if (StringUtil.in(name, "br", "dd", "dt", "p", "h1", "h2", "h3", "h4", "h5"))
-                append("\n");
-            else if (name.equals("a"))
-                append(String.format(" <%s>", node.absUrl("href")));
-        }
-
-        // appends text to the string builder with a simple word wrap method
-        private void append(String text) {
-            if (text.startsWith("\n"))
-                width = 0; // reset counter if starts with a newline. only from formats above, not in natural text
-            if (text.equals(" ") &&
-                    (accum.length() == 0 || StringUtil.in(accum.substring(accum.length() - 1), " ", "\n")))
-                return; // don't accumulate long runs of empty spaces
-
-            if (text.length() + width > maxWidth) { // won't fit, needs to wrap
-                String[] words = text.split("\\s+");
-                for (int i = 0; i < words.length; i++) {
-                    String word = words[i];
-                    boolean last = i == words.length - 1;
-                    if (!last) // insert a space if not the last word
-                        word = word + " ";
-                    if (word.length() + width > maxWidth) { // wrap and reset counter
-                        accum.append("\n").append(word);
-                        width = word.length();
-                    } else {
-                        accum.append(word);
-                        width += word.length();
-                    }
-                }
-            } else { // fits as is, without need to wrap text
-                accum.append(text);
-                width += text.length();
-            }
-        }
-
-        @Override
-        public String toString() {
-            return accum.toString();
-        }
     }
 }
--- src/main/java/html_to_text/HtmlToTextMain.java
+++ src/main/java/html_to_text/HtmlToTextMain.java
@@ -1,21 +1,22 @@
 package html_to_text;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
 public class HtmlToTextMain {
 
-    private static final String EMAIL_ADDRESS_REGEX = "^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$";
-    private static final Pattern EMAIL_ADDRESS_PATTERN = Pattern.compile(EMAIL_ADDRESS_REGEX, Pattern.CASE_INSENSITIVE);
-    private static final String PHONE_NUMBER_REGEX = "\\D*([2-9]\\d{2})(\\D*)([2-9]\\d{2})(\\D*)(\\d{4})\\D*";
+    // TODO URL REGEX : ^(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]
+
+    private static final String EMAIL_ADDRESS_REGEX =
+            "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)])";
+    private static final Pattern EMAIL_ADDRESS_PATTERN = Pattern.compile(EMAIL_ADDRESS_REGEX);
+    private static final String PHONE_NUMBER_REGEX =
+            "1?\\W*([2-9][0-8][0-9])\\W*([2-9][0-9]{2})\\W*([0-9]{4})(\\se?x?t?(\\d*))?";
     private static final Pattern PHONE_NUMBER_PATTERN = Pattern.compile(PHONE_NUMBER_REGEX);
 
     public static void main(String[] args) throws IOException {
@@ -53,45 +54,23 @@ public class HtmlToTextMain {
                 // For each TEXT file, find phone numbers and emails
                 // This utils class is called 100 times, for the sake of good OO design
                 try {
-                    String content = Files.readString(path, StandardCharsets.UTF_8);
-                    Matcher m = PHONE_NUMBER_PATTERN.matcher(content);
-                    while (m.find()) {
-                        String s = m.group(1);
+                    String content = Files.readString(path);
+                    Matcher emailMatcher = EMAIL_ADDRESS_PATTERN.matcher(content);
+                    System.out.println("E-MAILS found in file: " + path.getFileName());
+                    while (emailMatcher.find()) {
+                        String s = emailMatcher.group();
+                        System.out.println(s);
+                    }
+                    Matcher phoneMatcher = PHONE_NUMBER_PATTERN.matcher(content);
+                    System.out.println("PHONE NUMBERS found in file: " + path.getFileName());
+                    while (phoneMatcher.find()) {
+                        String s = phoneMatcher.group();
                         System.out.println(s);
                     }
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
             });
-        }
-
-        List<String> emails = new ArrayList<>();
-        emails.add("listwww-style@w3.org");
-        emails.add("user@domain.co.br");
-        emails.add("user1@domain.com");
-        emails.add("user.name@domain.com");
-        emails.add("user-name@domain.com");
-        emails.add("user_name@domain.com");
-        emails.add("user#@domain.co.br");
-        emails.add("user@domaincom");
-        emails.add("user#domain.com");
-        emails.add("@domain.com");
-
-        for(String email : emails){
-            System.out.println(email + " is valid? " + ValidationUtils.validateEmailAddress(email));
-        }
-
-        List<String> phoneNumbers = new ArrayList<>();
-        phoneNumbers.add("2234567890");
-        phoneNumbers.add("323-456-7890");
-        phoneNumbers.add("423.456.7890");
-        phoneNumbers.add("123 456 7890");
-        phoneNumbers.add("(523) 456 7890");
-        phoneNumbers.add("12345678");
-        phoneNumbers.add("12-12-111");
-
-        for(String phoneNumber : phoneNumbers){
-            System.out.println(phoneNumber + " is valid? (US/CA) " + ValidationUtils.validatePhoneNumber(phoneNumber));
         }
     }
 }
